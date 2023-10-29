841dbc700943519847dac675114492e34f80f889
maheshgodwani <143687176+maheshgodwani@users.noreply.github.com>
2023-10-29 11:51:23 +0530

    Update array_12.php

--- Array Exercise/array_12.php
+++ Array Exercise/array_12.php
@@ -3,8 +3,8 @@
 //12. Create an associative array representing a person's details, including name, age, and email.
 
 $personal_detail = array(
-    "name"=>"Rahul",
-    "age"=>"23",
-    "email"=>"rahulgautam7r@gmail.com");
+    "name"=>"Mahesh",
+    "age"=>"21",
+    "email"=>"mhsmca5723@gmail.com");
 print_r($personal_detail);
 ?>
