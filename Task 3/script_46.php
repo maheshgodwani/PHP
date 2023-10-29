9141742926b2663c157b6ad70d834cecc920dcf9
maheshgodwani <143687176+maheshgodwani@users.noreply.github.com>
2023-10-29 11:42:56 +0530

    Update script_46.php

--- Task 3/script_46.php
+++ Task 3/script_46.php
@@ -1,7 +1,7 @@
 <?php
 //script 46 to fill an array with a specific value of an  array using array_fill().
  
-$Name=array("Rahul","John","Vidhay","Yuvraj");
+$Name=array("Mahesh","Ajay","Bhavin","Kaushik");
 
 $Name=array_fill(5,1,"Ajay"); 
 print_r($Name);
