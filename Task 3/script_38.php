7b2fd9ecc6f164a80930f12037c0baaa65ecb42b
maheshgodwani <143687176+maheshgodwani@users.noreply.github.com>
2023-10-29 11:37:36 +0530

    Update script_38.php

--- Task 3/script_38.php
+++ Task 3/script_38.php
@@ -1,10 +1,10 @@
 <?php
 //script 38 to shift and unshift the elements in array using array_shift() & array_unshift().
 
-$Name=array("Rahul","John","Vidhay","Yuvraj");
+$Name=array("Mahesh","Ravi","Meet","Deep");
 print_r(array_shift($Name));
 
 echo "<br/>";
-array_unshift($Name,"Ajay");
+array_unshift($Name,"Ayush");
 print_r($Name); 
 ?>
