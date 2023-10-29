5fe8fdef56c40a2bcc5f8dd9238665ac309283bf
maheshgodwani <143687176+maheshgodwani@users.noreply.github.com>
2023-10-29 11:51:50 +0530

    Update array_13.php

--- Array Exercise/array_13.php
+++ Array Exercise/array_13.php
@@ -1,8 +1,8 @@
 <?php
 //13. Access and display the persons age from the associative array created in question 12.
 $personal_detail = array(
-    "name"=>"Rahul",
-    "age"=>"23",
-    "email"=>"rahulgautam7r@gmail.com");
+    "name"=>"Mahesh",
+    "age"=>"21",
+    "email"=>"mhsmca5723@gmail.com");
 echo "The age of the person is ".$personal_detail["age"];
 ?>
