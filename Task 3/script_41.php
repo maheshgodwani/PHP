74e6ffdc79c4e0bea39261134347fbadf9d83f49
maheshgodwani <143687176+maheshgodwani@users.noreply.github.com>
2023-10-29 11:39:19 +0530

    Update script_41.php

--- Task 3/script_41.php
+++ Task 3/script_41.php
@@ -1,8 +1,8 @@
 <?php
 //script 41 to find the intersection between two  array using array_intersect().
  
-$Name=array("Rahul","John","Vidhay","Yuvraj");
-$name2=array("Smith","Michael","Rahul");
+$Name=array("Mahesh","Abhi","Darshak","Darsh");
+$name2=array("Smith","Parth","Neel");
  
 echo "<br/>";
 $intersection_value=array_intersect($Name,$name2); 
