1caf9f4fe38351a361798042c13887c0e7caa79b
maheshgodwani <143687176+maheshgodwani@users.noreply.github.com>
2023-10-29 11:36:53 +0530

    Update script_37.php

--- Task 3/script_37.php
+++ Task 3/script_37.php
@@ -1,8 +1,8 @@
 <?php
 //script 37 to add and remove the elements in array using array_push() & array_pop().
 
-$Name=array("Rahul","John","Vidhay","Yuvraj");
-array_push($Name,"Krushal");
+$Name=array("Mahesh","Chiranj","Viral","Krish");
+array_push($Name,"Viraj");
 print_r($Name);
 echo "<br/>";
 array_pop($Name);
